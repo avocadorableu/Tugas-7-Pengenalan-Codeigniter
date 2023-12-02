ff4d054387282e19a7da065922c893e88c0b6a97
avocadorableu <119839071+avocadorableu@users.noreply.github.com>
2023-12-02 17:05:26 +0800

    Update about.php

--- app/application/views/about.php
+++ app/application/views/about.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>ABC Mart</title>
+    <title>Sunshine</title>
  
     <style type="text/css">
  
@@ -71,10 +71,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div id="body">
         <h3>Deskripsi</h3>
         <p>
-            Toko ini merupakan toko yang menjual barang-barang kebutuhan sehari-hari. Barang yang kami jual, seperti makanan, minuman, 
-            dan barang kebutuhan lainnya. ABC Mart juga buka selama 24 jam. 
+            Toko ini merupakan toko yang menjual barang-barang koleksi pribadi. Sunshine juga buka selama 24 jam. 
         </p>
-        <p>Tanggal Pembuatan : 16 Februari 2020</p>
+        <p>Tanggal Pembuatan : 2 Desember 2023</p>
     </div>
  
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
